c97247b9aa5c4fda5cfdd1ca50ddab8dacc324c3
Pavel Vedernikov <pvedernikov@gmail.com>
2022-11-16 23:23:59 +0100

    Created App

---
+++ MinimalApi/MinimalApiService/Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinimalApiService.Data.Models
+{
+    [Table("users")]
+    [DisplayName("Пользователи")]
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ MinimalApi/MinimalApiService/Data/ServiceDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiService.Data.Models;
+
+namespace MinimalApiService.Data
+{
+    public class ServiceDbContext : DbContext
+    {
+        public ServiceDbContext() { }
+
+        public ServiceDbContext(DbContextOptions<ServiceDbContext> options) : base(options) { }
+
+        DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=Database.db");
+        }
+    }
+}
